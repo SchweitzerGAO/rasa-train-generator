1df625b40aa9af5f2f22d0994bc8c8ba1729f005
Charles Gao <1030587461@qq.com>
2023-04-15 12:55:35 +0800

    first commit

---
+++ rasa_gen/generator.py
@@ -0,0 +1,20 @@
+class Template:
+    def __init__(self):
+        pass
+
+    def add_sentence(self):
+        pass
+
+    def add_word(self, ):
+        pass
+
+
+class Generator:
+    def __init__(self):
+        pass
+
+    def build(self):
+        pass
+
+    def generate(self, n):
+        pass
