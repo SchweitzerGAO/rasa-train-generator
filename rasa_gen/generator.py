763f616ea7bb57de73a978dc6f6dfc11d3072cd6
Charles Gao <1030587461@qq.com>
2023-04-15 23:01:19 +0800

    second commit

--- rasa_gen/generator.py
+++ rasa_gen/generator.py
@@ -1,20 +1,38 @@
+from typing import List, Union
+
+
 class Template:
     def __init__(self):
-        pass
+        self.sentences = []
+        self.words = []
 
-    def add_sentence(self):
-        pass
+    def add_sentence(self, sentence_template: Union[str, List[str]]):
+        if isinstance(sentence_template, str):
+            self.sentences.extend([sentence_template])
+        else:
+            self.sentences.extend(sentence_template)
+        return self
 
-    def add_word(self, ):
-        pass
+    def add_word(self, word_template: Union[str, List[str]]):
+        if isinstance(word_template, str):
+            self.words.extend([word_template])
+        else:
+            self.words.extend(word_template)
+        return self
 
 
 class Generator:
-    def __init__(self):
-        pass
+    def __init__(self, intent_name):
+        self.intent_name = intent_name
+        self.templates = []
 
-    def build(self):
-        pass
+    def add_template(self, templates: List[Template]):
+        self.templates.extend(templates)
+        return self
 
-    def generate(self, n):
-        pass
+    def generate(self, n, to_file):
+        sentences = set()
+
+
+
+
