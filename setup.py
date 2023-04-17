e6738ff61ac506592403698d40541663bd9b08c5
Charles Gao <1030587461@qq.com>
2023-04-17 13:30:44 +0800

    second commit

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="rasa_gen",  # 这里是pip项目发布的名称
-    version="0.0.1",  # 版本号，数值大的会优先被pip
-    keywords=["pip", "rasa_gen"],  # 关键字
+    name="rasa_gen",
+    version="0.0.1",
+    keywords=["pip", "rasa_gen"],
     description="Rasa train data generator",
     long_description="Rasa train data generator version 0.0.1",
-    license="MIT Licence",  # 许可证
+    license="MIT Licence",
 
-    url="https://github.com/Adenialzz/SongUtils",  # 项目相关文件地址，一般是github项目地址即可
-    author="A",  # 作者
-    author_email="********@***.com",
+    url="https://github.com/Adenialzz/SongUtils",
+    author="Charles Gao",
+    author_email="charlesgao2101024@gmail.com",
 
     packages=find_packages(),
     include_package_data=True,
